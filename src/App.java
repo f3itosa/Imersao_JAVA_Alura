a6a0a82b8fd72edd6bcafea8cf86866f2e4e975d
Eric Batista Feitosa <eric.feitosa@kyndryl.com>
2023-04-04 19:21:02 -0300

    Geracao de figurinha

---
+++ src/App.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // Cria diretorio de figurinhasWhats
+        var diretorio = new File("figurinhasWhats/");
+        diretorio.mkdir();
+
+        // exibir e manipular os dados 
+        var geradora = new GeradoraDeFigurinhas();
+        for (int index = 0; index < 5; index++) {
+            
+            var filme = listaDeFilmes.get(index);
+            
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+
+
+            String textoFigurinha;
+            InputStream imagemClassificacao; 
+            if (classificacao > 9.0 ) {
+                textoFigurinha = "FILME MEGA TOPZERA";
+                imagemClassificacao = new FileInputStream(new File("sobreposicao/Feliz.jpg"));
+            }else {
+                textoFigurinha = "Agradável...";
+                imagemClassificacao = new FileInputStream (new File("sobreposicao/Agradavel.jpg"));
+            }
+            
+
+
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = "figurinhasWhats/" + titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemClassificacao);
+
+            System.out.println(titulo);
+            System.out.println();
+        }
+    }
+}
