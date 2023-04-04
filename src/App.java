963ec737b2b291326c9aff29c8037da04e57f3f0
Eric Batista Feitosa <eric.feitosa@kyndryl.com>
2023-04-04 20:29:26 -0300

    Ajuste Figurinha

--- src/App.java
+++ src/App.java
@@ -52,7 +52,7 @@ public class App {
                 imagemClassificacao = new FileInputStream (new File("sobreposicao/Agradavel.jpg"));
             }
 
-            // Teste Branch v 
+            // Teste Branch 
             
             
 
